86876468de629aad76de52ecfb4104e8f5cce2a5
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-02-17 18:44:02 +0200

    Make total likes function and a test

--- tests/return1.test.js
+++ tests/return1.test.js
@@ -6,3 +6,46 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const blogList = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 50,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 500,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5000,
+      __v: 0
+    }
+  ]
+
+  test ('How many likes in total: ', () => {
+    const result = listHelper.totalLikes(blogList)
+
+    expect(result).toBe(5555)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let total = 0
+  for (let i = 0; i < blogs.length; i++) {
+    total += blogs[i].likes
+  }
+  return total
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
