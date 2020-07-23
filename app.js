517bebd157a61c4584db2b102cddb910db02a5ce
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-07-23 19:20:11 +0300

    Add an error handling middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 require('dotenv').config()
 
 
@@ -27,11 +28,16 @@ mongoose.connect(config.MONGODB_URI, {
 	})
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 
 module.exports = app
