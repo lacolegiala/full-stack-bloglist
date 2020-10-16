964fe988b69f64c2bd79a1dd9cf6933e01ac44fd
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-10-16 11:16:13 +0300

    Make a new endpoint to get individual users

--- controllers/users.js
+++ controllers/users.js
@@ -25,4 +25,13 @@ usersRouter.get('/', async (request, response) => {
   response.json(users.map(user => user.toJSON()))
 })
 
+usersRouter.get('/:id', async (request, response) => {
+  const user = await User.findById(request.params.id)
+  if (user) {
+		response.json(user.toJSON())
+	} else {
+		response.status(404).end()
+	}
+})
+
 module.exports = usersRouter
