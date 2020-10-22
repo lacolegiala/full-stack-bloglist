c9d761648eb5a1a89fa953e1c32b991d834bd6af
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-10-22 18:18:59 +0300

    Populate individual users

--- controllers/users.js
+++ controllers/users.js
@@ -20,14 +20,16 @@ usersRouter.post('/', async (request, response) => {
 })
 
 usersRouter.get('/', async (request, response) => {
+  console.log('everything found')
   const users = await User
     .find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
   response.json(users.map(user => user.toJSON()))
 })
 
 usersRouter.get('/:id', async (request, response) => {
-  const user = await User.findById(request.params.id)
+  const user = await User.findById(request.params.id).populate('blogs', {title: 1, author: 1, url : 1})
   if (user) {
+    console.log('user found', user)
 		response.json(user.toJSON())
 	} else {
 		response.status(404).end()
